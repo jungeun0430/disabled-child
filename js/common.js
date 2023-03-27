ea0d290367064ff05c67dd1bcc402f9c8ea63661
jungeun0430 <je.lee@iea.co.kr>
2023-03-28 08:59:29 +0900

    깃 수정반영(최신)

--- js/common.js
+++ js/common.js
@@ -1,4 +1,78 @@
 $(function(){
+
+    /* 사이드바 연결 */
+    if(!$("body").hasClass("no-commons")){
+        $("body").prepend(
+          "<div class='side-menu'>\n" +
+          "    <ul class='main-menu'>\n" +
+          "        <li><a href='#'><img src='../img/home-icon.svg' alt=''><img src='../img/home-wt-icon.svg' class='icon-focus'><span class='title only-tablet'>대시보드</span></a>\n" +
+          "        </li>\n" +
+          "        <li><a href=#><img src='../img/group-icon.svg' alt=''><img src='../img/group-wt-icon.svg' class='icon-focus'><span class='title only-tablet'>돌봄 서비스</span></a>\n" +
+          "            <ul class='second-menu'>\n" +
+          "                <li><a href='#'>배정조회</a></li>\n" +
+          "                <li><a href='#'>서비스 관리<img src='../img/down-arrow.svg'></a>\n" +
+          "                    <ul class='third-menu'>\n" +
+          "                        <li><a href='#'>서비스 조회</a></li>\n" +
+          "                        <li><a href='#'>월간 서비스 계획</a></li>\n" +
+          "                    </ul>\n" +
+          "                </li>\n" +
+          "            </ul>\n" +
+          "        </li>\n" +
+          "        <li><a href='#'><img src='../img/assign-icon.svg' alt=''><img src='../img/assign-wt-icon.svg' class='icon-focus'><span class='title only-tablet'>교육의수</span></a>\n" +
+          "            <ul class='second-menu'>\n" +
+          "                <li><a href='#'>교육이수현황 조회</a></li>\n" +
+          "            </ul>\n" +
+          "        </li>\n" +
+          "        <li><a href='#'><img src='../img/find-icon.svg' alt=''><img src='../img/find-wt-icon.svg' class='icon-focus'><span class='title only-tablet'>돌보미 급여조회</span></a>\n" +
+          "            <ul class='second-menu'>\n" +
+          "                <li><a href='#'>급여 조회</a></li>\n" +
+          "            </ul>\n" +
+          "        </li>\n" +
+          "        <li><a href='#'><img src='../img/note-icon.svg' alt=><img src='../img/note-wt-icon.svg' class='icon-focus'><span class='title only-tablet'>게시판</span></a>\n" +
+          "            <ul class='second-menu'>\n" +
+          "                <li><a href='#'>공지사항</a></li>\n" +
+          "                <li><a href='#'>커뮤니티</a></li>\n" +
+          "                <li><a href='#'>1:1문의</a></li>\n" +
+          "            </ul>\n" +
+          "        </li>\n" +
+          "        <li><a href='#'><img src='../img/setting-icon.svg' alt=''><img src='../img/setting-wt-icon.svg' class='icon-focus'><span class='title only-tablet'>관리자페이지</span></a>\n" +
+          "            <ul class='second-menu'>\n" +
+          "                <li><a href='#'>서비스현황<img src='../img/down-arrow.svg'></a>\n" +
+          "                    <ul class='third-menu'>\n" +
+          "                        <li><a href='#'>돌보미 정보</a></li>\n" +
+          "                        <li><a href='#'>이용자 정보</a></li>\n" +
+          "                        <li><a href='#'>서비스 모니터링</a></li>\n" +
+          "                    </ul>\n" +
+          "                </li>\n" +
+          "                <li><a href='#'>사업실적<img src='../img/down-arrow.svg'></a>\n" +
+          "                    <ul class='third-menu'>\n" +
+          "                        <li><a href='#'>지역 현황</a></li>\n" +
+          "                        <li><a href='#'>돌봄 서비스 현황</a></li>\n" +
+          "                        <li><a href='#'>장애아 돌보미 현황</a></li>\n" +
+          "                        <li><a href='#'>휴식지원 프로그램 현황</a></li>\n" +
+          "                        <li><a href='#'>연간 사업 이용만족도 통계</a></li>\n" +
+          "                        <li><a href='#'>돌보미 교육만족도 관리</a></li>\n" +
+          "                        <li><a href='#'>교육이수확인</a></li>\n" +
+          "                    </ul>\n" +
+          "                </li>\n" +
+          "                <li><a href='#'>시스템총괄<img src='../img/down-arrow.svg'></a>\n" +
+          "                    <ul class='third-menu'>\n" +
+          "                        <li><a href='#'>돌봄서비스 소개 관리</a></li>\n" +
+          "                        <li><a href='#'>휴식지원프로그램 소개 관리</a></li>\n" +
+          "                        <li><a href='#'>오시는 길 관리</a></li>\n" +
+          "                        <li><a href='#'>관련 사이트 관리</a></li>\n" +
+          "                        <li><a href='#'>시스템 파라미터 관리</a></li>\n" +
+          "                    </ul>\n" +
+          "                </li>\n" +
+          "                <li><a href='#'>메시지발송</a></li>\n" +
+          "            </ul>\n" +
+          "        </li>\n" +
+          "    </ul>\n" +
+          "    <div class=sub-menu-bg></div>\n" +
+          "</div>"
+        );
+    }
+
     /* 팝업 */
     $(".pop-btn").on("click", function(){
         $("body").css("overflow","hidden");
@@ -46,18 +120,37 @@ $(function(){
             $(".popup.pop21").addClass("on");
         }else if($(this).hasClass("pop22")){
             $(".popup.pop22").addClass("on");
+        }else if($(this).hasClass("pop23")){
+            $(".popup.pop23").addClass("on");
+        }else if($(this).hasClass("pop24")){
+            $(".popup.pop24").addClass("on");
+        }else if($(this).hasClass("pop25")){
+            $(".popup.pop25").addClass("on");
         }
     });
+
+
     $(".popup").on("click", function(e){
         if(e.target === this){
             $("body").css("overflow","auto");
             $(this).removeClass("on");
         }
     });
-    $(".popup .pop-close, .popup .ok-btn, .popup .close-btn").on("click", function(){
+    
+    $(".popup .pop-close, .popup .ok-btn, .popup , .close-btn , .store_btn ").on("click", function(){
         $("body").css("overflow","auto");
         $(this).parents(".popup").removeClass("on");
     });
+
+    // 저장 버튼 SA-08-10
+    // $('button.store-btn').on('click', function() {
+    //     $("div.popup.popup23, div.popup.popup24 ").addClass("on")
+    //     $('div.popup').on('scroll touchmove mousewheel', function(event) {
+    //         event.preventDefault();
+    //         event.stopPropagation();
+    //         return false;
+    //     });
+    // })
 
 
     // 수료증 출력
@@ -71,75 +164,127 @@ $(function(){
     // DatePicker
     $( ".datepicker" ).datepicker({
         dateFormat: 'yy.mm.dd',
-        showOn: "button",
-        buttonImage: "../img/down-arrow.svg",
-        buttonImageOnly: true,
-        buttonText: "Select date"
+        // showOn: "button",
+        // buttonImage: "../img/down-arrow.svg",
+        // buttonImageOnly: true,
+        // buttonText: "Select date"
+    });
+
+    // 삭제버튼 클릭시 다시확인창
+    $('.delete-btn').on('click', function() {
+        confirm('정말로 삭제하시겠습니까?');
     });
 
     // 사이드메뉴
-    $(".main-menu > li").on("click", function(){
-        $(this).find(".second-menu").toggleClass("on");
+    $(".second-menu").slideUp(0);
+    $(".third-menu").slideUp(0);
+    $('.div.sub-menu-bg').removeClass('on');
+
+    $("ul.main-menu > li > a").on("click", function(){
+        console.log('클릭중')
+        if($(this).next().length >= 1){
+            if(!$(this).hasClass("on")){
+                $('ul.main-menu > li > a').removeClass('on')
+                $(this).addClass("on");
+                $('ul.second-menu').removeClass('on').slideUp(300);
+                $(this).next().addClass('on').slideDown(300);
+                $('ul.second-menu > li > a').removeClass('on');
+                $('ul.third-menu').slideUp(0)
+                $('div.sub-menu-bg').addClass('on');
+
+            }else{
+                $(this).removeClass("on");
+                $('ul.second-menu').removeClass('on') 
+                $(this).next().removeClass('on').slideUp(300);
+                $('div.sub-menu-bg').removeClass('on');
+            }
+        } else {
+            $('ul.main-menu > li > a').removeClass('on')
+            $("ul.second-menu").removeClass('on').slideUp(300);
+            $('div.sub-menu-bg').removeClass('on');
+        }
     });
-    $(".second-menu > li").hover(function(){
-        $(this).find(".third-menu").stop().slideToggle(1000);
+
+    // 3뎁스
+    $("ul.second-menu > li > a").on("click", function() {
+        if($(this).next().length >= 1){
+            if(!$(this).hasClass("on")){
+                $('ul.second-menu > li > a').removeClass('on')
+                $(this).addClass("on");
+                $('ul.third-menu').slideUp(300);
+                $(this).next().slideDown(300);
+
+            }else{
+                $(this).removeClass("on");
+                $('ul.second-menu > li > a').removeClass('on')
+                $(this).next().slideUp(300);
+            }
+        }
+
+        // 스크롤 자동 밑으로 이동
+        $(".side-menu").animate({
+
+
+            
+            scrollTop: $('.side-menu').height()
+        }, 1000);
     });
-    
 
+    // $('div.side-menu').on('scroll touchmove mousewheel',  function(event) {
+    //     event.preventDefault();
+    //     event.stopPropagation();
+    //     return false;
+    // });
 
+     //모바일버전
+    // width사이즈를 측정해서 resize 체크해야함.
+    var screenWidth = $(window).width();
 
-    console.log($(".second-menu > li"))
+    if (screenWidth > 1440) {
+        $('div.side-menu').removeClass('on');
+    }
 
-    
-        // $('ul.main-menu > li > a').on('click', function() {
-        //     var index = $(this).parent().index();
-        //     $('ul.third-menu').removeClass('on');
-        //     $('div.sub-menu-bg').toggleClass('on');
-        //     $('ul.second-menu:eq('+ (index - 1) +')').toggleClass('on');
+    $('a.hamburger-button').on('click', function() {
+        $('body').css({overflowY:'hidden'});
+        $('div.side-menu').toggleClass('on');
+        if($('div.side-menu').hasClass('on')) {
+            $('a.hamburger-button').addClass('close');
+            
+        } else {
+            $('body').css({overflowY:'auto'});
+            $('a.hamburger-button').removeClass('close');
+            // 열고 닫을 때 화면 정리해주기
+            $('div.side-menu > ul.main-menu > li > a').removeClass('on')
+            $('ul.second-menu').slideUp(500)
+            $('ul.second-menu > li > a').removeClass('on');
+            $('ul.third-menu').slideUp(500);
+        }
+    });
 
-        //     해당하는 ul.second가 있으면 보여주기
-        //     if($('ul.main-menu > li:eq('+ index +') >ul.second-menu').length) {
-        //         // $('ul.second-menu').removeClass('on');
-        //         // $('ul.second-menu:eq('+ (index - 1) +')').addClass('on');
-        //         // $('ul.third-menu').removeClass('on');
-        //         // $('div.sub-menu-bg').addClass('on');
+    $(window).resize(function() {
+        var screenWidth = $(window).width();
 
-        //         두번클릭했을 때 사이드 메뉴가 닫혀야 함
-        //         if($('ul.second-menu:eq('+ (index - 1) +')').outerHeight(true)) {
-        //             console.log('dkfjkdjk')
-        //             $('ul.main-menu > li > a').on('click', function() {
-        //                 console.log('dksjfkjafdkljsdlkjkldsjflksjdkfj');
-        //                 // $('ul.second-menu:eq('+ (index - 1) +')').toggleClass('on');
-        //                 // $('ul.third-menu').toggleClass('on');
-        //                 // $('div.sub-menu-bg').toggleClass('on');
-        //             })
-        //         }
+        if(screenWidth > 1400) {
+            // 1400 이상이면 모바일 버전 정리해
+            $('div.side-menu').removeClass('on');
+            $('div.sub-menu-bg').removeClass('on');
+            $('a.hamburger-button').removeClass('close');
+            resetNav();
+        } else {
+            // 1400보다 작으면 pc버전 정리해
+            $('div.sub-menu-bg').removeClass('on');
+            resetNav();
+        }
 
+        
+    });
 
-
-         // 3번째 뎁스
-        //         $('ul.second-menu:eq('+ (index - 1) +') > li > a').on('click', function() {
-        //         var idx = $(this).parent().index();
-        //             if(index === 1) {
-        //                 if($('ul.second-menu:eq('+ (index - 1) +') > li:eq('+idx+') > ul.third-menu').length) {
-        //                     $('ul.third-menu').addClass('on');
-        //                 } else {
-        //                     $('ul.third-menu').removeClass('on');
-        //                 }
-
-        //             } else {
-        //                 $('ul.second-menu:eq('+ (index - 1) +') > li > ul.third-menu').removeClass('on');
-        //                 $('ul.second-menu:eq('+ (index - 1) +') > li > ul.third-menu:eq('+idx+')').addClass('on');
-        //             }
-        //         });
-        //     } else {
-        //         $('div.sub-menu-bg').removeClass('on')
-        //         $('ul.second-menu').removeClass('on');
-        //         $('ul.third-menu').removeClass('on');
-        //     }
-        // });
-
-
-    
+    function resetNav() {
+        $('div.side-menu > ul.main-menu > li > a').removeClass('on')
+        $('ul.second-menu').removeClass('on');
+        $('ul.second-menu').slideUp(500);
+        $('ul.second-menu > li > a').removeClass('on');
+        $('ul.third-menu').slideUp(500);  
+    }
 
 });
---
+++ js/user_common.js
@@ -0,0 +1,225 @@
+$(function(){
+    $(document).ready(function(){
+        /* 넘버리스트 숫자넣기 */
+        $(".front-number > li").each(function () {
+            let thisIndex = $(this).index() + 1;
+            $(this).attr("data-content", (thisIndex+")"));
+        });
+    });
+
+    // 헤더 자동으로 넣기
+    $('body').prepend(`
+        <header id="header">
+            <div class="content-box j-between a-center">
+            <a href="#" title="hamburger-button" class="hamburger-button only-tablet on"><span class="bar">메뉴 열고/닫기</span></a>
+            <h1>
+                <a href="#">
+                    <img src="../img/logo.svg" alt="장애아가족 양육지원사업"/>
+                </a>
+            </h1>
+            <div class="menu-wrap j-between a-center">
+                <ul class="menu">
+                    <li>
+                        <a href="#">장애아 양육지원사업</a>
+                        <ul class="second-depth">
+                            <li>
+                                <a href="#">장애아가족 양육지원사업</a>
+                                <ul class="third-depth">
+                                    <li><a href="#">목적 및 추진방향</a></li>
+                                    <li><a href="#">연혁</a></li>
+                                    <li><a href="#">사업내용</a></li>
+                                    <li><a href="#">근거법령 및 추진체계</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#">사업현황</a>
+                                <ul class="third-depth">
+                                    <li><a href="#">돌봄 서비스</a></li>
+                                    <li><a href="#">휴식지원 프로그램</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#">사업시행 기관</a>
+                            </li>
+                            <li>
+                                <a href="#">오시는 길</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">돌봄서비스</a>
+                        <ul class="second-depth">
+                            <li><a href="#">지원대상</a></li>
+                            <li><a href="#">지원내용</a></li>
+                            <li><a href="#">선정기준 및 이용 절차</a></li>
+                            <li><a href="#">이용요금</a></li>
+                            <li><a href="#">서비스 중지</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">휴식지원 프로그램</a>
+                        <ul class="second-depth">
+                            <li><a href="#">지원대상</a></li>
+                            <li><a href="#">지원내용</a></li>
+                            <li><a href="#">선정기준 및 이용 절차</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">장애아 돌보미</a>
+                        <ul class="second-depth">
+                            <li><a href="#">자격기준 및 양성교육</a></li>
+                            <li><a href="#">선발 및 활동절차</a></li>
+                            <li><a href="#">주요활동</a></li>
+                            <li><a href="#">근무조건</a></li>
+                            <li><a href="#">연계중지 및 활동중지</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">정보마당</a>
+                        <ul class="second-depth">
+                            <li><a href="#">공지사항</a></li>
+                            <li><a href="#">갤러리</a></li>
+                            <li><a href="#">자주 묻는 질문</a></li>
+                            <li><a href="#">자료실</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                <div class="member-box">
+                    <button type="button" class="btn line">로그인</button>
+                    <button type="button" class="btn line">서비스</button>
+                </div>
+            </div>
+            </div>
+        </header>
+
+    `);
+
+    // footer 자동으로 넣기
+    $('body').append(`
+        <footer id="footer">
+        <div class="content-box j-between">
+            <div class="btn-wrap a-center">
+                <button type="button" class="btn foot">개인정보처리방침</button>
+                <button type="button" class="btn foot">저작권정책</button>
+            </div>
+            <div class="footer-info-box">
+                <ul class="footer-info-list">
+                    <li>[07236] 서울시 영등포구 의사당대로 22 이룸센터 7층 중앙장애아동, 발달장애인지원센터</li>
+                    <li>대표전화   02-3433-0743</li>
+                    <li>발달재활서비스 제공인력 자격관리   1544-6065</li>
+                    <li>팩스   02-3433-0739</li>
+                    <li>발달장애인 권리구제 상담   1522-2882</li>
+                </ul>
+                <p class="copyright">COPYRIGHTS⒞ 2017 NATIONAL CENTER FOR DISABLED CHILDREN AND DEVELOPMENTAL DISABILITIES. ALL RIGHTS RESERVED</p>
+            </div>
+            <div class="select-box a-center">
+                <div class="select">
+                <select name="" id="">
+                    <option value="0">관련사이트</option>
+                </select>
+                </div>
+            </div>
+        </div>
+    </footer>
+    `);
+
+
+
+
+
+
+    /* 팝업 열기 */
+    $(".pop-btn").on("click", function() {
+        $("body").addClass("on");
+        if ($(this).hasClass("pop1")) {
+            $(".popup.pop1").addClass("on");
+        }
+    });
+
+    /* 팝업 닫기 (배경 선택) */
+    $(".popup").on("click", function(e){
+        if(e.target === this){
+            $("body").removeClass("on");
+            $(this).removeClass("on");
+        }
+    });
+
+    /* 팝업 닫기 버튼 */
+    $(".popup .pop-close, .popup .close-btn").on("click", function(){
+        $("body").css("overflow","auto");
+        $(this).parents(".popup").removeClass("on");
+    });
+
+    /* 탭 버튼 */
+    $(".tab-wrap .btn").on("click", function(){
+        let tabNum = $(this).parent().index();
+        $(".tab-wrap .btn").removeClass("on").parent().eq(tabNum).find(".btn").addClass("on");
+        $(".tab-box .tabs").removeClass("on").eq(tabNum).addClass("on");
+    });
+
+
+    // 반응형 헤더: 햄버거 버튼
+    $('a.hamburger-button').on('click', function() {
+        $('body').css({overflowY:'hidden'});
+        $('div.menu-wrap').toggleClass('on');
+        if($('div.menu-wrap').hasClass('on')) {
+            $('a.hamburger-button').addClass('close');
+            
+        } else {
+            $('body').css({overflowY:'auto'});
+            $('a.hamburger-button').removeClass('close');
+            // 열고 닫을 때 화면 정리해주기
+            $('.menu > li > a').removeClass('on')
+            $('ul.second-depth').slideUp(500)
+            $('ul.second-depth > li > a').removeClass('on');
+            $('ul.third-depth').slideUp(500);
+
+        }
+    });
+
+    $(".second-depth, .third-depth").slideUp(0);
+
+    $(".menu > li > a, .second-depth a").on("click", function(){
+        if($(window).innerWidth() <= 1350){
+            if(!$(this).next().hasClass("on")){
+                $(this).next().addClass("on").slideDown();
+            }else{
+                $(this).next().removeClass("on").slideUp();
+            }
+        }
+    });
+
+    let windowWidth = $(window).innerWidth();
+
+    if( windowWidth  < 1350){
+        $('.login-small-box').slideUp(0);
+    }
+
+    $(window).resize(function(){
+        if($(window).innerWidth() >= 1350){
+            $('.hamburger-button').removeClass('close');
+            $(".second-depth, .third-depth").removeClass("on").stop().slideUp(0);
+            $('.login-small-box').slideDown(0);
+            $(".login-btn").removeClass("open");
+        }else{
+            $('.login-small-box').slideUp(0);
+        }
+    });
+
+
+    $('.login-btn').on("click",function(){
+        if(!$(this).hasClass('open')){
+            $(this).addClass("open");
+            $('.login-small-box').slideDown(200);
+        }else{
+            $(this).removeClass("open");
+            $('.login-small-box').slideUp(200);
+        }
+    });
+
+
+
+
+
+
+});
